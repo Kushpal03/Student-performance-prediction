b51ee00dcadf9967fbb4fd8d98cb25eacebae088
Kush Pal <151172645+Kushpal03@users.noreply.github.com>
2026-04-26 02:50:21 +0530

    Added Template downloader , file formatting rules

--- pages/bulk.py
+++ pages/bulk.py
@@ -6,9 +6,57 @@ import joblib
 model=joblib.load('model.pkl')
 st.set_page_config(page_title='Bulk Predict',initial_sidebar_state='collapsed')
 
-if st.button('🛖 Back to Home'):
-    st.switch_page("deploy.py")
+template_data = pd.DataFrame({
+        'Name' : ['Example Student'],
+        'gender':['Male'],
+        'age':[15],
+        'parental_education_level':[5],
+        'family_income':[100000],
+        'daily_study_hours':[5.5],
+        'attendance_rate':[0.85],
+        'sleep_hours':[8.5],
+        'stress_level':[2],
+        'motivation_score':[78],
+        'private_tutoring':[True],
+        'internet_quality':[5]
+    })
 
+csv_template = template_data.to_csv(index=False).encode('utf-8') 
+col1,col2 = st.columns(2,gap='small')
+
+with col1:
+
+    if st.button('🛖 Back to Home',use_container_width=True):
+        st.switch_page("deploy.py")
+with col2:
+    st.download_button(label='⬇️ Download Blank CSV Template',use_container_width=True,
+                       file_name='student_prediction_template.csv',
+                       data=csv_template,
+                       mime='text/csv'
+                       )
+
+with st.expander('📝 Click here to view CSV formatting rules.'):
+    st.write('**Please ensure your uploaded CSV has these exact columns :**')
+    rul_col1,rul_col2 = st.columns(2)
+    with rul_col1:
+        
+        st.markdown("""
+                    * **gender:** Must be typed exactly as Male or Female.
+                    * **age:** Must be an integer.
+                    * **parental_education_level:** Must be a whole number between 1 and 9.
+                    * **family_income:** Must be a whole number.
+                    * **daily_study_hours:** Must be a decimal upto 1 decimal place.
+                    * **attendance_rate:** Must be between 0 and 1.
+                    """)
+    with rul_col2:
+        st.markdown("""
+                    * **stress_level:** Must be an integer between 0 and 10.
+                    * **motivation_score:** Must be whole number between 0 and 100.
+                    * **sleep_hours:** Must be a decimal upto 1 decimal place.
+                    * **internet_quality:** Must be a whole number between 1 and 5.
+                    * **private_tutoring:** Must be typed exactly as True or False.
+                    """)
+    st.caption('*(Note: Any additional columns present in your file need not be removed they can be uploaded as is .The system automatically ignores any additional columns.)*')
 @st.cache_data
 def load_data(file):
     time.sleep(1)
@@ -64,16 +112,18 @@ if uploaded_file is not None:
 
             csv_results = final_results.to_csv(index=False).encode('utf-8')
             
+            res_col1,res_col2=st.columns(2,gap='small',vertical_alignment='bottom')
 
-            
-            res_file_name = st.text_input('Enter the Result File Name',value='Class Results')
-            
-            if not res_file_name.endswith('.csv'):
-                final_file_name = res_file_name + '.csv'
-            else:
-                final_file_name = res_file_name
+            with res_col1:
 
-            st.download_button(label='⬇️ Download the Results for the Class',
-                            data=csv_results ,
-                            file_name=final_file_name,
-                            mime='text/csv')
+                res_file_name = st.text_input('Enter the Result File Name',value='Class Results')
+                
+                if not res_file_name.endswith('.csv'):
+                    final_file_name = res_file_name + '.csv'
+                else:
+                    final_file_name = res_file_name
+            with res_col2:
+                st.download_button(label='⬇️ Download the Results for the Class',
+                                data=csv_results ,
+                                file_name=final_file_name,
+                                mime='text/csv',use_container_width=True)
